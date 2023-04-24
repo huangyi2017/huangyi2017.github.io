095f976f3549c22c3e9448134c14cee0ac20e7bb
huangyi <841213272@qq.com>
2023-04-24 10:37:12 +0800

    111

---
+++ docs/assets/js/answer.js
@@ -0,0 +1,8 @@
+<script type="javascript/text">
+   window.onload = function(){
+        let aa = document.getElementById("aa")
+        aa.onclick = function(){
+        alert("aaa")
+        }
+   }
+</script>
